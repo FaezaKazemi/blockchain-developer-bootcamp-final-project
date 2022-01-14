223ec5cc77c840cc5b1eda2cec113279b5b23ff5
FaezaKazemi <faeza.kazeme1998@gmail.com>
2022-01-14 09:51:40 -0800

    my deployment

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/blockchain-developer-bootcamp-final-project/"
-      : "/"
-};
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/blockchain-developer-bootcamp-final-project/'
+    : '/'
+}
